7bc3791f86fe6b63502701d7742a6e5b9b0a7c0f
omnema007 <nemaom2003@gmail.com>
2025-02-21 16:44:57 +0530

    [Om] Add. Create a Helloworld App to display in REST Call

---
+++ HelloWorld/src/main/java/com/demo/uc3/HelloRestApplication.java
@@ -0,0 +1,12 @@
+package com.demo.uc3;
+
+import com.demo.uc2.Application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloRestApplication.class, args);
+    }
+}
---
+++ HelloWorld/src/main/java/com/demo/uc3/HelloRestController.java
@@ -0,0 +1,12 @@
+package com.demo.uc3;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from BridgeLabz";
+    }
+}
